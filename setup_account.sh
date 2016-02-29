4e1eda93698f58a184d2afd6e73d77b27e2f39d7
Tsan-Kuang Lee <github@tklee.com>
2016-02-29 14:47:42 -0500

    another typo

--- setup_account.sh
+++ setup_account.sh
@@ -3,9 +3,9 @@
 user="$1"
 if [ $user == "root" ]
 then
-    userdir="/home/$user"
-else
     userdir="/root"
+else
+    userdir="/home/$user"
 fi
 rsync -at .bashrc .bash_profile .vim* .tmux.conf .config $userdir
 chown -R $user:$user $userdir
