18fa0576467ff2a20a59bab7a3393ace7ca8dd14
Tsan-Kuang Lee <github@tklee.com>
2016-02-29 13:19:06 -0500

    update to support root dir

--- setup_account.sh
+++ setup_account.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 
 user="$1"
-rsync -at .bashrc .bash_profile .vim* .tmux.conf .config /home/$user
-chown -R $user:$user /home/$user
+if [ $user -eq "root" ]
+then
+    userdir="/home/$user"
+else
+    userdir="/root"
+fi
+rsync -at .bashrc .bash_profile .vim* .tmux.conf .config $userdir
+chown -R $user:$user $userdir
 
