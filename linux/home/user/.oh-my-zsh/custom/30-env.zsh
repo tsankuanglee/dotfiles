2421a3a622ca01249fc5abaed86ac7d43c7c1ef2
Tsan-Kuang Lee <tsan.kuang.lee@gmail.com>
2023-08-13 12:54:02 -0500

    add perl for nvim; comment about fcitx env

--- linux/home/user/.oh-my-zsh/custom/30-env.zsh
+++ linux/home/user/.oh-my-zsh/custom/30-env.zsh
@@ -37,6 +37,13 @@ export SDL_IM_MODULE=${IME}
 # Do not set GTK_IM_MODULE to xim globally as it affects GTK3 programs as well.
 #export GTK_IM_MODULE=xim
 
+# Perl
+PATH="~/perl5/bin${PATH:+:${PATH}}"; export PATH;
+PERL5LIB="~/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
+PERL_LOCAL_LIB_ROOT="~/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
+PERL_MB_OPT="--install_base \"~/perl5\""; export PERL_MB_OPT;
+PERL_MM_OPT="INSTALL_BASE=~/perl5"; export PERL_MM_OPT;
+
 
 export PATH=$PATH:~/.local/bin:~/.cargo/bin
 # remove duplicates
