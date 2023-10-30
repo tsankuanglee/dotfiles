dfacf958a52a3b9a0646f700854ade2ec5c494f3
Tsan-Kuang Lee <tsan.kuang.lee@gmail.com>
2023-10-30 02:53:42 -0500

    nvim: quicklist plugins

---
+++ linux/home/user/.config/nvim.lazy/lua/plugins/ui/unimpaired.lua
@@ -0,0 +1,11 @@
+-- https://github.com/tummetott/unimpaired.nvim
+-- provides convience in keybinds
+
+return {
+    'tummetott/unimpaired.nvim',
+    config = function()
+        require('unimpaired').setup {
+            -- add any options here or leave empty
+        }
+    end
+}
