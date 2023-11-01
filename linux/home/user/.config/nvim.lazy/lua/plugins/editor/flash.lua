3e48c291846ce5c8a913cc4e876da3940fe7bc2e
Tsan-Kuang Lee <tlee@myriad.com>
2023-11-01 15:30:37 -0500

    nvim: flash search default to off

--- linux/home/user/.config/nvim.lazy/lua/plugins/editor/flash.lua
+++ linux/home/user/.config/nvim.lazy/lua/plugins/editor/flash.lua
@@ -1,13 +1,19 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    modes = {
+      search = {
+        enabled = false, -- default off; use <c-s> to toggle
+      },
+    },
+  },
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "<C-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 }
