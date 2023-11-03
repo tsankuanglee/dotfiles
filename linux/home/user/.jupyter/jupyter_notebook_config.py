45e9bdda8286810c3e48b79064381fc7425707b4
Tsan-Kuang Lee <tsan.kuang.lee@gmail.com>
2023-11-03 15:26:07 -0500

    jupyter: put the two files without (fake) credentials back in, so scanner does not complain

---
+++ linux/home/user/.jupyter/jupyter_notebook_config.py
@@ -0,0 +1,6 @@
+c.NotebookApp.mathjax_url = "https://cdn.mathjax.org/mathjax/latest/MathJax.js"
+c.NotebookApp.iopub_data_rate_limit = 10000000000
+
+# It was a real fake hash here, but the security scanner doesn't like it, so we make it into a comment.
+# c.NotebookApp.password = 'fake hash'
+
