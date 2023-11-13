560bbf2377da8b352536d13f039350e6a54d0f9d
Tsan-Kuang Lee <tsan.kuang.lee@gmail.com>
2023-11-12 21:53:45 -0600

    nvim: nap: my PR got merged, update back to upstream; add more keys to recession

--- linux/home/user/.config/nvim.lazy/lua/plugins/ui/nap.lua
+++ linux/home/user/.config/nvim.lazy/lua/plugins/ui/nap.lua
@@ -1,7 +1,7 @@
 -- nap: next and previous; a cleaner version of vim unimpaired
 return {
   "liangxianzhe/nap.nvim",
-  lazy = false,
+  lazy = false, -- read early so others can overwrite
   config = function()
     local nap = require("nap")
     -- The provided implementation falls back to ]c [c in diff mode.
@@ -10,14 +10,19 @@ return {
       prev_prefix = "[",
       next_repeat = "<c-n>",
       prev_repeat = "<c-p>",
+      exclude_default_operators = {
+        "a",
+        "A",
+        "b",
+        "B",
+      },
     })
     -- To remove a default operator
     --nap.map("a", false)
 
     -- overwrite with external tools
     nap.map("c", nap.gitsigns())
-    nap.map('o', nap.aerial())
-    nap.map('r', nap.illuminate())
-
+    nap.map("o", nap.aerial())
+    nap.map("r", nap.illuminate())
   end,
 }
--- linux/home/user/.config/nvim.lazy/lua/plugins/utility/resession.lua
+++ linux/home/user/.config/nvim.lazy/lua/plugins/utility/resession.lua
@@ -8,6 +8,7 @@ return {
     -- Resession does NOTHING automagically, so we have to set up some keymaps
     local km = require("utils").km -- keymap shortcut function
     km("n", "<leader>ss", resession.save, "[reSession save]")
+    km("n", "<leader>st", resession.save_tab, "[reSession save_tab]")
     km("n", "<leader>sl", resession.load, "[reSession load]")
     km("n", "<leader>sd", resession.delete, "[reSession delete]")
   end,
