6338ffe014218948b361c7968d39246de4d26428
Tsan-Kuang Lee <tsan.kuang.lee@gmail.com>
2023-11-13 19:24:38 -0600

    nvim: add pyright config

--- linux/home/user/.config/nvim.lazy/lua/plugins/lsp/nvim-lspconfig.lua
+++ linux/home/user/.config/nvim.lazy/lua/plugins/lsp/nvim-lspconfig.lua
@@ -10,18 +10,19 @@ return {
     require("stylua-nvim").setup({ config_file = "stylua.toml" })
 
     local lspconfig = require("lspconfig")
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
     lspconfig.lua_ls.setup({
-      --[[ commands = {
-        -- Format = {
-        --   function()
-        --     require("stylua-nvim").format_file()
-        --   end,
-        -- },
-      }, ]]
+      commands = {
+        -- external formatter
+        Format = {
+          function() require("stylua-nvim").format_file() end,
+        },
+      },
       capabilities = capabilities,
+      filetypes = { "lua" },
       settings = {
         Lua = {
+          -- internal formatter
           format = {
             enable = false,
             -- NOTE: the value should be String!
@@ -37,6 +38,12 @@ return {
         },
       },
     })
+    local config = require("plugins.lsp.nvim-lspconfig")
+    lspconfig.pyright.setup({
+      on_attach = config.on_attach,
+      capabilities = capabilities,
+      filetypes = { "python" },
+    })
 
     -- keymaps
     local km = require("utils").km -- keymap shortcut function
@@ -49,7 +56,7 @@ return {
     km("n", "<LEADER>lh", vim.lsp.buf.hover, "hover/help")
 
     km("n", "<LEADER>lR", vim.lsp.buf.rename, "refactor/rename")
-    km("n", "<F2>", vim.lsp.buf.rename, "refactor/rename")
+    -- km("n", "<F2>", vim.lsp.buf.rename, "refactor/rename")
 
     km("n", "<LEADER>lc", vim.lsp.buf.code_action, "code_action")
 
