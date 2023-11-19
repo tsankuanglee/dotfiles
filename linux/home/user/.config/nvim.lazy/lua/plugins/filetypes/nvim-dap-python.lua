c7b6c4b893f29a2075ab6752d2e75cdb20cc36bc
Tsan-Kuang Lee <tsan.kuang.lee@gmail.com>
2023-11-19 15:17:50 -0600

    nvim: more hotkeys for nvim-dap-python and treesj

--- linux/home/user/.config/nvim.lazy/lua/plugins/filetypes/nvim-dap-python.lua
+++ linux/home/user/.config/nvim.lazy/lua/plugins/filetypes/nvim-dap-python.lua
@@ -8,10 +8,13 @@ return {
   config = function()
     local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
     local dp = require("dap-python")
+    dp.test_runner = 'pytest'
     dp.setup(path)
 
     local km = require("utils").km -- keymap shortcut function
-    km("n", "<LOCALLEADER>dyr", function () dp.test_method() end, "[DAP-python] run test_method")
+    km("n", "<LOCALLEADER>dyr", function () dp.test_method() end, "[DAP-python] test_method")
+    km("n", "<LOCALLEADER>dyf", function () dp.test_class() end, "[DAP-python] test_class")
+    km("v", "<LOCALLEADER>dys", function () dp.debug_selection() end, "[DAP-python] debug_selection test_class")
 
   end,
 }
--- linux/home/user/.config/nvim.lazy/lua/plugins/programming/treesj.lua
+++ linux/home/user/.config/nvim.lazy/lua/plugins/programming/treesj.lua
@@ -6,10 +6,14 @@ return {
     local tsj = require("treesj")
     tsj.setup({
       use_default_keymaps = false,
+      -- max_join_length = 120,
     })
     local km = require("utils").km -- keymap shortcut function
       km('n', '<LOCALLEADER>jt', require('treesj').toggle, "TreeSJ toggle")
+      km('n', '<LOCALLEADER>jT', function () require('treesj').toggle({ split = { recursive = true } }) end, "TreeSJ toggle recursively")
       km('n', '<LOCALLEADER>jj', require('treesj').join, "TreeSJ join")
+      km('n', '<LOCALLEADER>jJ', function () require('treesj').join({ split = { recursive = true } }) end, "TreeSJ join recursively")
       km('n', '<LOCALLEADER>js', require('treesj').split, "TreeSJ split")
+      km('n', '<LOCALLEADER>jS', function () require('treesj').split({ split = { recursive = true } }) end, "TreeSJ split recursively")
   end,
 }
