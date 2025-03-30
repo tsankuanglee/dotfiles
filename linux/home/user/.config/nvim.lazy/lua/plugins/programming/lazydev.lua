cb72fee6d0e1b3723be7311f5d44389e8254f450
Tsan-Kuang Lee <1425438+tsankuanglee@users.noreply.github.com>
2025-03-29 19:30:07 -0500

    nvim: fix lazydev

--- linux/home/user/.config/nvim.lazy/lua/plugins/programming/lazydev.lua
+++ linux/home/user/.config/nvim.lazy/lua/plugins/programming/lazydev.lua
@@ -2,7 +2,7 @@ return {
   {
     "folke/lazydev.nvim",
     ft = "lua", -- only load on lua files
-    lazy = true,
+    --lazy = true,
     -- setup is in nvim-lspconfig.lua
     opts = {
       library = {
@@ -21,23 +21,23 @@ return {
         group_index = 0, -- set group index to 0 to skip loading LuaLS completions
       })
     end,
-  },
-  { -- optional blink completion source for require statements and module annotations
-    "saghen/blink.cmp",
-    opts = {
-      sources = {
-        -- add lazydev to your completion providers
-        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            -- make lazydev completions top priority (see `:h blink.cmp`)
-            score_offset = 100,
-          },
-        },
-      },
-    },
   }
+  --{ -- optional blink completion source for require statements and module annotations
+  --  "saghen/blink.cmp",
+  --  opts = {
+  --    sources = {
+  --      -- add lazydev to your completion providers
+  --      default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+  --      providers = {
+  --        lazydev = {
+  --          name = "LazyDev",
+  --          module = "lazydev.integrations.blink",
+  --          -- make lazydev completions top priority (see `:h blink.cmp`)
+  --          score_offset = 100,
+  --        },
+  --      },
+  --    },
+  --  },
+  --}
   -- { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
 }
