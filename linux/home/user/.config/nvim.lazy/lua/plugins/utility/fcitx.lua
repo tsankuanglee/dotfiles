c1a620921891de30e2f8ce753d17e42b9800b85d
Tsan-Kuang Lee <1425438+tsankuanglee@users.noreply.github.com>
2025-07-15 02:14:41 -0500

    nvim: add fcitx config comment

--- linux/home/user/.config/nvim.lazy/lua/plugins/utility/fcitx.lua
+++ linux/home/user/.config/nvim.lazy/lua/plugins/utility/fcitx.lua
@@ -1,7 +1,9 @@
+-- auto switch input method between insert/normal mode
 return {
   "h-hg/fcitx.nvim",
-  version = false,
-  -- lazy = true,
+  version = nil,
+  -- set lazy to false since this is ubiquitous
+  lazy = false,
   config = function()
   end,
 }
