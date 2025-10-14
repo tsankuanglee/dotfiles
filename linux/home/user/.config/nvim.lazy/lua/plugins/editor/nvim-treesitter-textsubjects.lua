c96fc5c067dede4b8d9c1f8d790c8e9343b04882
Tsan-Kuang Lee <1425438+tsankuanglee@users.noreply.github.com>
2025-10-14 11:57:47 -0500

    nvim: update nvim-treesitter-textsubjects

--- linux/home/user/.config/nvim.lazy/lua/plugins/editor/nvim-treesitter-textsubjects.lua
+++ linux/home/user/.config/nvim.lazy/lua/plugins/editor/nvim-treesitter-textsubjects.lua
@@ -1,19 +1,22 @@
 -- Location and syntax aware text objects which *do what you mean*
+-- keys: (after v, V, C-V, etc.)
+-- ;  will select a syntactical container (class, function, etc.) depending on your location in the syntax tree.
+-- i; will select the body of a syntactical container depending on your location in the syntax tree.
+-- .  will select the most relevant part of the syntax tree depending on your location in it.
+-- ,  to repeat the last selection.
 return {
   "RRethy/nvim-treesitter-textsubjects",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   event = "VeryLazy",
   lazy = true,
   config = function()
-    require("nvim-treesitter.configs").setup({
-      textsubjects = {
-        enable = true,
-        prev_selection = "<C-p>", -- (Optional) keymap to select the previous selection
-        keymaps = {
-          ["@"] = "textsubjects-smart",
-          ["_"] = "textsubjects-container-outer",
-          ["-"] = { "textsubjects-container-inner", desc = "Select inside containers (classes, functions, etc.)" },
-        },
+    local textsubjects = require("nvim-treesitter-textsubjects")
+    textsubjects.configure({
+      prev_selection = ',',
+      keymaps = {
+        ['.'] = 'textsubjects-smart',
+        [';'] = 'textsubjects-container-outer',
+        ['i;'] = 'textsubjects-container-inner',
       },
     })
   end,
