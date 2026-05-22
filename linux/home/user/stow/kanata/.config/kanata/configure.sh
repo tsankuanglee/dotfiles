a55eaf81fa403aeb837cb49b94fd2dc532ecefa7
Tsan-Kuang Lee <1425438+tsankuanglee@users.noreply.github.com>
2026-05-22 11:22:48 -0500

    kanata: organize and mac support

--- linux/home/user/stow/kanata/.config/kanata/configure.sh
+++ linux/home/user/stow/kanata/.config/kanata/configure.sh
@@ -12,18 +12,45 @@ else
 fi
 
 # CREATE actual config files from templates
-# service file
+mkdir -p ./local/{common,layouts,scripts,user_defined}
+
+# {{{ service files
+# linux systemd service files
 mkdir -p ~/.config/systemd/user
-envsubst '$KANATA_BIND_ADDRESS,$KANATA_BIND_PORT' < ./kanata.service.tmpl > ~/.config/systemd/user/kanata.service
+envsubst '$KANATA_BIND_ADDRESS,$KANATA_BIND_PORT' < ./general/system_files/kanata.service.tmpl > ~/.config/systemd/user/kanata.service
+# macos daemon file
+#envsubst '$KANATA_USER,$KANATA_CONFIG_HOME' < ./general/system_files/Library/LaunchDaemons/com.kanata.daemon.plist.tmpl > ./local/com.kanata.daemon.plist
+# }}}  service files
+#
+# {{{ kanata app files
 
-# toggle script
-envsubst '$KANATA_BIND_ADDRESS,$KANATA_BIND_PORT' < ./kanata-toggle.sh.tmpl > ./local/kanata-toggle.sh
-chmod a+x ./local/kanata-toggle.sh
+#  {{{ common
+envsubst '$LINUX_DEV,$DANGER_ENABLE_CMD' < ./general/common/defcfg.kbd.tmpl > ./local/common/defcfg.kbd
+envsubst '$KANATA_CONFIG_HOME,$KANATA_USER' < ./general/common/deftemplates.kbd.tmpl > ./local/common/deftemplates.kbd
+#  }}} common
 
+#  {{{ layouts
+rsync -at ./general/layouts/${LAYOUTS_FILENAME} ./local/layouts/
+#  }}} layouts
 
-mkdir -p ./local
-envsubst '$LINUX_DEV,$DANGER_ENABLE_CMD' < ./common/defcfg.kbd.tmpl > ./local/defcfg.kbd
-envsubst '$RUN_EXTERNAL_COMMAND_AS' < ./common/deftemplates.kbd.tmpl > ./local/deftemplates.kbd
-envsubst '$LAYOUTS_FILENAME,$INCLUDE_SEQUENCES,$INCLUDE_MACROS,$INCLUDE_CHORDS' < ./kanata.kbd.tmpl > ./local/kanata.kbd
-envsubst '$USER' < system_files/Library/LaunchDaemons/com.kanata.daemon.plist > ./local/com.kanata.daemon.plist
+#  {{{ scripts
+# layer toggle (default <-> passthrough) script; to effectively disable kanata before screen lock (so no hotkeys can keep launching new process)
+envsubst '$KANATA_BIND_ADDRESS,$KANATA_BIND_PORT' < ./general/scripts/kanata-toggle.sh.tmpl > ./local/scripts/kanata-toggle.sh
+# macos service launcher
+envsubst '$KANATA_CONFIG_HOME' < ./general/scripts/macos-service.sh.tmpl > ./local/scripts/macos-service.sh
+# macos app launcher
+envsubst '$KANATA_USER' < ./general/scripts/macos-app-launcher.sh.tmpl > ./local/scripts/kanata-app-launcher.sh
+
+chmod a+x ./local/scripts/*.sh
+#  }}} scripts
+
+#  {{{ user_defined
+rsync -at ./general/user_defined/ ./local/user_defined/
+#  }}} user_defined
+
+#  {{{ main config
+envsubst '$LAYOUTS_FILENAME,$INCLUDE_SEQUENCES,$INCLUDE_MACROS,$INCLUDE_CHORDS' < ./general/kanata.kbd.tmpl > ./local/kanata.kbd
+#  }}} main config
+
+# }}} kanata app files
 
